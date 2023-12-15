c65285308ce2568979591254d5f2c8819e2388ff
wmicknick <132094991+wmicknick@users.noreply.github.com>
2023-12-15 09:15:16 -0500

    Add files via upload

--- tester.py
+++ tester.py
@@ -1,22 +1,32 @@
 import unittest
-from budget import BudgetCalculator
+from budget_final import BudgetCalculator
 
-class Test(unittest.TestCase):
+class TestBudgetCalculator(unittest.TestCase):
     def test_expense_proportions(self):
-        budget = BudgetCalculator(income=5000, hours=160, rent=1000, car=500, gas=200, food=500, subscriptions=100, savings=1000, goal=30)
-        expense_pie_chart, greatest_expense = budget.expense_proportions()
-        self.assertEqual(expense_pie_chart['Rent'], 20)
+        budget = BudgetCalculator(income=2500, hours=40, rent=1500, car=400, gas=250, food=150, subscriptions=50, savings=100, goal=30)
+        expense_pie_chart, greatest_expense, free_money_proportion = budget.expense_proportions()
+        self.assertEqual(expense_pie_chart['Rent'], 60)
+        self.assertEqual(free_money_proportion, 2)
         self.assertEqual(greatest_expense, 'Rent')
 
     def test_calculate_hourly_wage(self):
-        budget = BudgetCalculator(income=5000, hours=160, rent=1000, car=500, gas=200, food=500, subscriptions=100, savings=1000, goal=30)
-        hourly_wage = budget.calculate_hourly_wage()
-        self.assertEqual(hourly_wage, 31.25)
+        budget = BudgetCalculator(income=2500, hours=40, rent=1500, car=400, gas=250, food=150, subscriptions=50, savings=100, goal=30)
+        hourly_wage = budget.hourly_wage()
+        hourly_wage_trim = f"{hourly_wage:.2f}"
+        self.assertEqual(hourly_wage_trim, '62.50')
 
     def test_calculate_daily_budget(self):
-        budget = BudgetCalculator(income=5000, hours=160, rent=1000, car=500, gas=200, food=500, subscriptions=100, savings=1000, goal=30)
-        daily_allowance = budget.calculate_daily_budget()
-        self.assertEqual(daily_allowance, 10.0)
+        budget = BudgetCalculator(income=2500, hours=40, rent=1500, car=400, gas=250, food=150, subscriptions=50, savings=100, goal=30)
+        daily_allowance = budget.daily_budget()
+        daily_allowance_trim = f"{daily_allowance:.2f}"
+        self.assertEqual(daily_allowance_trim, '1.67')
+
+    def test_allowance_goal_difference(self):
+        budget = BudgetCalculator(income=2500, hours=40, rent=1500, car=400, gas=250, food=150, subscriptions=50, savings=100, goal=30)
+        daily_allowance = budget.daily_budget()
+        deficit_to_goal = budget.allowance_goal_difference(30, daily_allowance)
+        deficit_to_goal_trim = f"{deficit_to_goal:.2f}"
+        self.assertEqual(deficit_to_goal_trim, '28.33')
 
 if __name__ == '__main__':
     unittest.main()
